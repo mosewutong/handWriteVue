28a86905f806dd50cfd83137a9564117e5211463
墨色梧桐 <liyi_cdut@163.com>
2019-05-05 18:30:37 +0800

    2019-5-5更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
 let path = require('path');
 let webpack = require('webpack');
 
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
     entry: './src/main.js', // 项目的入口文件，webpack会从main.js开始，把所有依赖的js都加载打包
     output: {
-        path: path.resolve(__dirname, './dist'), // 项目的打包文件路径
+        path: path.resolve(__dirname, './dist'), // 项目的打包文件路径   __dirname当前文件的绝对路径
         publicPath: '/dist/', // 通过devServer访问路径
         filename: 'build.js' // 打包后的文件名
     },
     //启动一个web服务器
     devServer: {
         historyApiFallback: true,   //遇到404重定向到index.html
-        overlay: true,
+        overlay: true,    //将错误显示在html之上
         host:"0.0.0.0",
         port:"7000",
         hot:true,   //热刷新
         inline: true,   //内联模式
         noInfo: false,   //只保留错误警告
-    }
+    },
+
+    plugins:[
+        // new UglifyJsPlugin()  //代码压缩
+    ]
 };
